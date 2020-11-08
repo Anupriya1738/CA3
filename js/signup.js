71e582c58c10168a84d0c396b6ac5dafea4b7bf4
unknown <anupriya1738@gmail.com>
2020-11-08 22:16:02 +0530

    Changes

--- js/signup.js
+++ js/signup.js
@@ -1,6 +1,16 @@
 $(document).ready(function () {
     $(".submit").click(function (e) {
         e.preventDefault();
+        let email = $("#email").val();
+        let pass = $("#pass").val();
+        console.log(email.length);
+        if( email.length === 0) {
+            console.log("returned")
+            return;
+        }
+        if(pass.length === 0) {
+            return;
+        }
         var data = JSON.parse(window.localStorage.getItem("accounts"));
         if (data === null) {
             data = [];
@@ -10,7 +20,7 @@ $(document).ready(function () {
         const data2 = {};
         form.map(function (item) {
             data2[item.name] = item.value;
-        });
+        }); 
         data.push(data2);
         localStorage.setItem("accounts", JSON.stringify(data));
         alert("Account created. Visit account page to view all accounts");
